feab55e3698b325e037cd4345ee2144851927821
Ekkon <muriloghignattI-@hotmail.com>
2019-10-16 14:24:07 -0300

    Fixed Conceitual error

--- Trabalho 8/src/ExecaoAutenticacao.java
+++ Trabalho 8/src/ExecaoAutenticacao.java
@@ -1,2 +1,9 @@
 public class ExecaoAutenticacao extends ExecaoSeguranca{
+    public ExecaoAutenticacao(String mensagem){
+        super(mensagem);
+    }
+
+    public ExecaoAutenticacao(){
+        super();
+    }
 }
--- Trabalho 8/src/ExecaoSeguranca.java
+++ Trabalho 8/src/ExecaoSeguranca.java
@@ -1,2 +1,9 @@
 public class ExecaoSeguranca extends Exception{
+    public ExecaoSeguranca(String mensagem){
+        super(mensagem);
+    }
+
+    public ExecaoSeguranca(){
+        super();
+    }
 }
