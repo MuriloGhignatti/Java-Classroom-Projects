b3bc93546fc12ec22b9c858c9e2654702a2eb916
MuriloGhignatti <muriloghignattI-@hotmail.com>
2019-10-22 10:23:29 -0300

    Added Login System

--- SecurePassword/src/SecurePassword.java
+++ SecurePassword/src/SecurePassword.java
@@ -1,11 +1,18 @@
 public class SecurePassword {
     public static void main(String[] args) throws Exception{
-        Usuario Dani = new Usuario("Daniela","Xablau");
-        String Texto = "Opa";
-        byte[] Cifrado = Dani.Encrypt(Texto);
-        String Decifrado = Dani.Decrypt(Cifrado);
-        Sys.Login("Daniela","Xablau");
-        Dani.ImprimirUK();
-        System.out.println("Texto Original: " + Texto + "\n Texto Cifrado: " + Cifrado + "\n Texto Decifrado: " + Decifrado);
+        try{
+            Usuario Dani = new Usuario("Daniela","Xablau");
+            String Texto = "Opa";
+            byte[] Cifrado = Dani.Encrypt(Texto);
+            String Decifrado = Dani.Decrypt(Cifrado);
+            Sys.Login("Daniela","Xablau");
+            Dani.ImprimirUK();
+            System.out.println("Texto Original: " + Texto + "\n Texto Cifrado: " + Cifrado + "\n Texto Decifrado: " + Decifrado);
+        }
+
+        catch (ExecaoContaExistente e){
+            System.out.println("Conta Já Existe");
+        }
+
     }
 }
